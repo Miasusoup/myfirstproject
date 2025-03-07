25b5fd5a4fa33edb19663a26e243a73e26d29d97
Mia <Murphy_Ahmed-M@ulster.ac.uk>
2025-03-07 22:24:53 +0000

    Fixed code

---
+++ myFirstProject/resources/resources/views/Components/nav-link.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<body>
+        <nav>
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+            <a href="/contact">Contact</a>
+        </nav>
+        
+    
+    </body>
+
+    {{$slot}}
---
+++ myFirstProject/resources/resources/views/home.blade.php
@@ -0,0 +1,7 @@
+<x-nav-link>
+    
+</x-nav-link>
+<x-layout>
+    <h1> Hello World </h1>
+</x-layout>
+
